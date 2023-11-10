7d8c22d81402005ac91c389443684ba7180303b9
Sena <Sena@ARMAPRCCIFOD549.FormacionSena.RED>
2023-11-10 11:12:29 -0500

    finished

---
+++ src/components/General/MouseBackDrop.tsx
@@ -0,0 +1,35 @@
+export const MouseBackDrop = () => {s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  return (
+    <div className="w-3 h-3 absolute shadow-[0_0_50px_white]"/>
+  )
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,13 +7,14 @@ export default {
         primary: "#252525",
         contrast: "#934141",
         secondary: "#5ED8FF",
+        third: "#BA20D3"
       },
       textColor: {
         principalText: "#FFF",
         secondaryText: "#000",
       },
       backgroundImage: {
-        primaryGradient: "linear-gradient(135deg , #0C171E, #262626)",
+        primaryGradient: "linear-gradient(135deg , #0C171E 30%, #262626 90%)",
       },
       borderRadius: {
         normal: "50px",
